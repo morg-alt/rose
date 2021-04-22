ca5c4a910c02c51c6542078ed5f022b8be519561
morg-alt <77060836+morg-alt@users.noreply.github.com>
2021-04-21 22:44:56 -0500

    Update nav.js

--- nav.js
+++ nav.js
@@ -1,29 +1,29 @@
-const hamburger = document.querySelector(".hamburger"); 
+const hamburger = document.querySelector(".hamburger");
 
-const navLinks = document.querySelector(".nav-links"); 
+const navLinks = document.querySelector(".nav-links");
 
-const links = document.querySelectorAll(".nav-links li"); 
+const links = document.querySelectorAll(".nav-links li");
 
- 
- 
 
-hamburger.addEventListener('click', ()=>{ 
 
-   //Animate Links 
 
-    navLinks.classList.toggle("open"); 
+hamburger.addEventListener('click', () => {
 
-    links.forEach(link => { 
+    //Animate Links 
 
-        link.classList.toggle("fade"); 
+    navLinks.classList.toggle("open");
 
-    }); 
+    links.forEach(link => {
 
- 
- 
+        link.classList.toggle("fade");
+
+    });
+
+
+
 
     //Hamburger Animation 
 
-    hamburger.classList.toggle("toggle"); 
+    hamburger.classList.toggle("toggle");
 
-}); 
+});
